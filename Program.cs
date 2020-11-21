84cd8be44440a2646405a357753dc7cc2bd172af
vivesamj <vivesamj@emea.roche.com>
2020-11-21 13:26:38 +0100

    New project + GetWordsCount method

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "Hello World! my friends.";
+            Console.WriteLine(s);
+            Console.WriteLine(s.GetWordCount());
+        }
+    }
+
+    public static class MyExtensions 
+    {
+        public static int GetWordCount(this String str)
+        {
+            var arrayWords = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return arrayWords.Length;
+        }
+    }
+}
